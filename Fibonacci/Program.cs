c5260e20f08145150bc2e30978de4d6613a52311
Lucas-M3dina <Lucas.m3dina@gmail.com>
2022-07-06 19:07:38 -0300

    Exercicios concluidos

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿int numeroAnterior = 0;
+int numeroAtual = 1;
+int fibonacci;
+bool loop = true;
+
+void Fibonacci(int numeroRecebido)
+{
+    while (loop)
+    {
+        fibonacci = numeroAnterior + numeroAtual;
+        numeroAnterior = numeroAtual;
+        numeroAtual = fibonacci;
+        if (fibonacci == numeroRecebido)
+        {
+            Console.WriteLine($"\n O numero {numeroRecebido} existe na sequencia Fibonacci");
+            loop = false;
+        }
+        else if (numeroRecebido < fibonacci)
+        {
+            Console.WriteLine($"\n O numero {numeroRecebido} não existe na sequencia Fibonacci");
+            loop = false;
+        }
+    }
+}
+
+
+Console.Write("Digite um numero para verificar se ele esta presente na sequencia de Fibonacci: ");
+Fibonacci(int.Parse(Console.ReadLine()));
---
+++ InvertendoString/Program.cs
@@ -0,0 +1,15 @@
+﻿string recebidoUsuario;
+char[] recebidoArray;
+string invertida = "";
+
+Console.Write("Digite uma palavra a ser invertida: ");
+recebidoUsuario = Console.ReadLine();
+
+recebidoArray = recebidoUsuario.ToCharArray();
+
+for (int i = recebidoArray.Length - 1; i >= 0; i--)
+{
+    invertida += recebidoArray[i].ToString();
+}
+
+Console.Write(invertida);
